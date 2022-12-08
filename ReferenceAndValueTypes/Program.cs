89d670495fc85994a53648981e8cc70848377740
Chetan Badgujar <Chetan.Badgujar@klingelnberg.com>
2022-12-08 12:47:35 +0530

    Value and refernce types

---
+++ ReferenceAndValueTypes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValueTypes
+{
+    internal class Program
+    {
+        string book;
+       
+
+        public Program (string book)
+        {
+            this.book = book;
+        }   
+
+        public void  ValueAsRefernce(ref int a)
+        {
+            a++;
+        }
+        public void ValueAsRefernce2(out int a)
+        {
+            a = 10;
+        }
+
+
+
+        static void Main(string[] args)
+        {   //pass by refernce
+            Program p = new Program("atomic habbits");
+            Program p2 = p;
+            p2.book = "attitude is everything";
+            Console.WriteLine(p.book);
+
+            //pass by value
+            int a = 7;
+            int b = a;
+            b = 5;
+            Console.WriteLine(a);
+
+            //pass value as refrence with ref
+            p.ValueAsRefernce( ref a);
+
+            Console.WriteLine(a);
+
+            //pass value as refrence with in;
+
+            p.ValueAsRefernce2(out a);
+
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+
+        }
+    }
+}
