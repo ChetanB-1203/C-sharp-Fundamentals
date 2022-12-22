7d2d44e30d306499f5d159b631502cea63da8af2
Chetan Badgujar <Chetan.Badgujar@klingelnberg.com>
2022-12-22 15:57:59 +0530

    exception handling

---
+++ ExceptionExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int div = 0;
+            try
+            {
+                div = 100 / x;
+                Console.WriteLine("Not executed line");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("you are dividing by zero");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("exception occured");
+            }
+            finally
+            {
+                Console.WriteLine("finally block");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
